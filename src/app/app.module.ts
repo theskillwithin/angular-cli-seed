4d2e43470ffc130ef2f51b1384c0e0f93f7e8a7c
A. Vatsaev <azero79@gmail.com>
2016-10-20 01:07:08 +0200

    cleanup and materialize css setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 
 import {AppRoutes} from './app.routing'
+import { MaterializeModule } from "angular2-materialize";
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    MaterializeModule,
     BrowserModule,
     FormsModule,
     HttpModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,11 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+
+  say_hello(){
+    alert("Hello World")
+  }
+
   ngOnInit() {
   }
 
